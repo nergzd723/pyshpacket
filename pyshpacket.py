dcf4a29d2cac24fdcbf6cf768b9f1e3f2cd49804
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-21 13:45:32 +0300

    Update pyshpacket.py

--- pyshpacket.py
+++ pyshpacket.py
@@ -4,7 +4,6 @@ import hashlib
 cntr = 0
 def encrypting(data):
     alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    len('fire')
 def getmda5(data):
     mda = hashlib.md5()
     data1 = data.encode('utf-8')
